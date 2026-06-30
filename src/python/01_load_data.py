15958e633c56981e6181b9bf81aa3fe712bf3d36
FranciszekMierzejewski <franek.mski@gmail.com>
2026-06-30 22:56:31 +0100

    Add .env file to contain database password, created database connection

--- src/python/01_load_data.py
+++ src/python/01_load_data.py
@@ -1,3 +1,31 @@
 import pandas as pd
+import psycopg2
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
 df = pd.read_excel("data/Online Retail.xlsx")
+
+df.columns = [
+    "invoice_no",
+    "stock_code",
+    "description",
+    "quantity",
+    "invoice_date",
+    "unit_price",
+    "customer_id",
+    "country"
+]
+
+df = df.fillna(None)
+data = list(df.to_records(index=False)) # convert df to numpy record array
+
+conn = psycopg2.connect(
+    dbname = "retail_db",
+    user = "postgres",
+    password = os.getenv("DB_PASSWORD"),
+    host = "localhost",
+    port = "5432"
+)
+
+cur = conn.cursor()
