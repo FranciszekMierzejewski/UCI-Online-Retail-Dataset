e97ada5ec40549bfaf6bc597d026034acd24b0f4
FranciszekMierzejewski <franek.mski@gmail.com>
2026-06-30 22:04:33 +0100

    Create retail_db database and raw_transactions table

--- src/python/01_load_data.py
+++ src/python/01_load_data.py
@@ -1,3 +1,3 @@
 import pandas as pd
 
-df = pd.read_excel("../../data/Online Retail.xlsx")
+df = pd.read_excel("data/Online Retail.xlsx")
---
+++ src/sql/01_create_staging.sql
@@ -0,0 +1,10 @@
+CREATE TABLE raw_transactions (
+    invoice_no TEXT,
+    stock_code TEXT,
+    description TEXT,
+    quantity TEXT,
+    invoice_date TEXT,
+    unit_price TEXT,
+    customer_id TEXT,
+    country TEXT
+)
