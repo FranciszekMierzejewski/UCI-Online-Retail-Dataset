f380a0e38676817fba9b680c8a25daed89f8e8ce
FranciszekMierzejewski <franek.mski@gmail.com>
2026-07-01 15:53:19 +0100

    Altered queries with more general statistics

--- src/sql/02_profile.sql
+++ src/sql/02_profile.sql
@@ -1,17 +1,3 @@
--- Total row count
-SELECT 
-    COUNT(*) AS number_of_rows 
-FROM 
-    raw_transactions;
-
--- Total count of empty customer_id
-SELECT 
-    COUNT(*) AS number_of_missing_customers
-FROM 
-    raw_transactions 
-WHERE 
-    customer_id IS NULL;
-
 -- Number of cancellations
 SELECT 
     COUNT(*) AS number_of_cancellations
@@ -19,3 +5,52 @@ FROM
     raw_transactions
 WHERE
     invoice_no LIKE 'C%'; -- Cancelled orders have invoice_no starting with C
+
+
+-- Table not including cancelled orders
+DROP VIEW IF EXISTS non_cancelled_orders;
+
+CREATE VIEW non_cancelled_orders AS 
+SELECT *
+FROM 
+    raw_transactions
+WHERE
+    invoice_no NOT LIKE 'C%';
+
+
+-- Number of sales that went ahead by stock_code
+SELECT
+    stock_code,
+    COUNT(*) AS number_of_successful_sales
+FROM 
+    non_cancelled_orders
+GROUP BY
+    stock_code
+ORDER BY
+    number_of_successful_sales DESC
+LIMIT
+    10;
+
+
+-- Row count
+SELECT
+    rt.total_number_of_rows,
+    rt.number_of_missing_rows,
+    rt.number_of_filled_rows,
+    rt.number_of_unique_customers,
+    nco.number_of_unique_customers_that_completed_orders
+FROM (
+    SELECT
+        COUNT(*) AS total_number_of_rows,
+        SUM(CASE WHEN customer_id IS NULL THEN 1 ELSE 0 END) AS number_of_missing_rows,
+        SUM(CASE WHEN customer_id IS NOT NULL THEN 1 ELSE 0 END) AS number_of_filled_rows,
+        COUNT(DISTINCT customer_id) AS number_of_unique_customers
+    FROM raw_transactions
+) rt
+CROSS JOIN (
+    SELECT
+        COUNT(DISTINCT customer_id) AS number_of_unique_customers_that_completed_orders
+    FROM non_cancelled_orders
+) nco;
+
+
